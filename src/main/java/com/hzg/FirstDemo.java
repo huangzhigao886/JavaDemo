1776b3e91584b50a68fc64d7f1e8794010812f45
huangzhigao <840983151@qq.com>
2019-11-25 01:00:07 +0800

    第一次使用git

---
+++ src/main/java/com/hzg/FirstDemo.java
@@ -0,0 +1,11 @@
+package com.hzg;
+
+/**
+ * @Author: huangzhigao
+ * @Date: 2019/11/25 0:56
+ */
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
